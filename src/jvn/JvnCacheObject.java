2a4ef7bb5a984707b606fcc83534185fce041666
Agustin <alarreine@gmail.com>
2017-10-06 01:40:15 +0200

    Create the object structure of the cache

--- src/jvn/JvnCacheObject.java
+++ src/jvn/JvnCacheObject.java
@@ -1,15 +1,78 @@
 package jvn;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
 
 public class JvnCacheObject {
 
     private JvnObject object;
-    private HashMap<JvnState,List<JvnRemoteServer>> listClient;
+    private HashMap<JvnState, List<JvnRemoteServer>> stateClient;
+    private Serializable latesContent = null;
 
 
+    public JvnCacheObject(JvnObject object, JvnState initialState, JvnRemoteServer initialServer) {
+        this.object = object;
+        this.stateClient = new HashMap<>();
+        List<JvnRemoteServer> listClient = new ArrayList<>();
+        listClient.add(initialServer);
+        this.stateClient.put(initialState, listClient);
+    }
 
+    /**
+     * Return object
+     * @return Object instance
+     */
+    public JvnObject getObject() {
+        return object;
+    }
 
+    /**
+     * Set Object
+     * @param object
+     */
+    public void setObject(JvnObject object) {
+        this.object = object;
+    }
+
+    /**
+     * Get List of Client that are using the object with an state
+     * @param state
+     * @return List object
+     */
+    public List<JvnRemoteServer> getRemoteServerByState(JvnState state) {
+        return stateClient.get(state);
+    }
+
+    /**
+     * Add Referece of the remote client
+     * @param state State of the object in the client side
+     * @param client reference of the client
+     */
+    public void setRemoteClientByState(JvnState state, JvnRemoteServer client) {
+        if (stateClient.containsKey(state)) {
+            stateClient.get(state).add(client);
+        } else {
+            List<JvnRemoteServer> listClient = new ArrayList<>();
+            listClient.add(client);
+            stateClient.put(state, listClient);
+        }
+    }
+
+    /**
+     * Get the last updated content of the object
+     * @return
+     */
+    public Serializable getLatesContent() {
+        return latesContent;
+    }
+
+    /**
+     * Set the last content of the object
+     * @param latesContent
+     */
+    public void setLatesContent(Serializable latesContent) {
+        this.latesContent = latesContent;
+    }
 }
--- src/jvn/impl/JvnCoordImpl.java
+++ src/jvn/impl/JvnCoordImpl.java
@@ -9,74 +9,85 @@
 package jvn.impl;
 
 import jvn.JvnCacheObject;
-import jvn.exception.JvnException;
 import jvn.JvnObject;
 import jvn.JvnRemoteCoord;
 import jvn.JvnRemoteServer;
+import jvn.exception.JvnException;
 
+import java.io.Serializable;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.io.Serializable;
+import java.util.Hashtable;
+import java.util.logging.Logger;
 
 
 public class JvnCoordImpl
         extends UnicastRemoteObject
         implements JvnRemoteCoord {
 
+    private static int nextObjectId;
+
     private static Registry registry;
     private static JvnCoordImpl coord;
+    private static Hashtable<Integer, JvnCacheObject> mainCache;
+
+    public static Logger logger = Logger.getLogger("jvn.impl.JvnCoordImpl");
 
 
     public static void main(String argv[]) throws Exception {
         try {
             if (System.getSecurityManager() == null) {
-                System.setProperty("java.security.policy","file:./java.policy");
+                System.setProperty("java.security.policy", "file:./java.policy");
                 System.setSecurityManager(new SecurityManager());
 
             }
-
+            //Initialization
             registry = LocateRegistry.createRegistry(1099);
-
             coord = new JvnCoordImpl();
+            mainCache = new Hashtable<>();
+
 
             System.out.println("Coordinator ready");
 
 
-
-
-        }catch (Exception e){
+        } catch (Exception e) {
+            logger.severe("Error while initiating Coordinator. " + e.getMessage());
 
         }
     }
+
     /**
      * Default constructor
+     *
      * @throws JvnException
      **/
     private JvnCoordImpl() throws Exception {
         try {
-            registry.bind("Coordinator",this);
+            nextObjectId = -1;
+            registry.rebind("Coordinator", this);
 
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
 
 
-
     /**
-     *  Allocate a NEW JVN object id (usually allocated to a
-     *  newly created JVN object)
+     * Allocate a NEW JVN object id (usually allocated to a
+     * newly created JVN object)
+     *
      * @throws java.rmi.RemoteException,JvnException
      **/
     public int jvnGetObjectId()
-            throws java.rmi.RemoteException,JvnException {
+            throws java.rmi.RemoteException, JvnException {
         // to be completed
         return 0;
     }
 
     /**
      * Associate a symbolic name with a JVN object
+     *
      * @param jon : the JVN object name
      * @param jo  : the JVN object
      * @param joi : the JVN object identification
@@ -84,51 +95,55 @@ public class JvnCoordImpl
      * @throws java.rmi.RemoteException,JvnException
      **/
     public void jvnRegisterObject(String jon, JvnObject jo, JvnRemoteServer js)
-            throws java.rmi.RemoteException,JvnException {
+            throws java.rmi.RemoteException, JvnException {
         // to be completed
     }
 
     /**
      * Get the reference of a JVN object managed by a given JVN server
+     *
      * @param jon : the JVN object name
-     * @param js : the remote reference of the JVNServer
+     * @param js  : the remote reference of the JVNServer
      * @throws java.rmi.RemoteException,JvnException
      **/
     public JvnObject jvnLookupObject(String jon, JvnRemoteServer js)
-            throws java.rmi.RemoteException,JvnException {
+            throws java.rmi.RemoteException, JvnException {
         // to be completed
         return null;
     }
 
     /**
      * Get a Read lock on a JVN object managed by a given JVN server
+     *
      * @param joi : the JVN object identification
      * @param js  : the remote reference of the server
      * @return the current JVN object state
      * @throws java.rmi.RemoteException, JvnException
      **/
     public Serializable jvnLockRead(int joi, JvnRemoteServer js)
-            throws java.rmi.RemoteException, JvnException{
+            throws java.rmi.RemoteException, JvnException {
         // to be completed
         return null;
     }
 
     /**
      * Get a Write lock on a JVN object managed by a given JVN server
+     *
      * @param joi : the JVN object identification
      * @param js  : the remote reference of the server
      * @return the current JVN object state
      * @throws java.rmi.RemoteException, JvnException
      **/
     public Serializable jvnLockWrite(int joi, JvnRemoteServer js)
-            throws java.rmi.RemoteException, JvnException{
+            throws java.rmi.RemoteException, JvnException {
         // to be completed
         return null;
     }
 
     /**
      * A JVN server terminates
-     * @param js  : the remote reference of the server
+     *
+     * @param js : the remote reference of the server
      * @throws java.rmi.RemoteException, JvnException
      **/
     public void jvnTerminate(JvnRemoteServer js)
