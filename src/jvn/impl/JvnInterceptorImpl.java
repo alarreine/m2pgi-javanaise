cd8294c5c6a9f6bb4815addfb13c4859c75a0e7e
Agustin <alarreine@gmail.com>
2017-10-09 09:41:40 +0200

    Fix Bug ObjectInterceptor State assignment

--- src/jvn/impl/JvnInterceptorImpl.java
+++ src/jvn/impl/JvnInterceptorImpl.java
@@ -7,118 +7,118 @@ import jvn.exception.JvnException;
 import java.io.Serializable;
 import java.util.concurrent.locks.ReentrantLock;
 
-import static jvn.JvnState.*;
+import static jvn.JvnState.R;
 
 public class JvnInterceptorImpl implements JvnObject {
 
-	JvnState state;
-	private int id;
-	private Serializable managedObject;
+    JvnState state;
+    private int id;
+    private Serializable managedObject;
 
-	private ReentrantLock lockState = new ReentrantLock();
+    private ReentrantLock lockState = new ReentrantLock();
 
-	public JvnInterceptorImpl(int id, Serializable managedObject) {
-		this.id = id;
-		this.managedObject = managedObject;
-		this.state = JvnState.W;
-	}
+    public JvnInterceptorImpl(int id, Serializable managedObject) {
+        this.id = id;
+        this.managedObject = managedObject;
+        this.state = JvnState.W;
+    }
 
-	@Override
-	public void jvnLockRead() throws JvnException {
+    @Override
+    public void jvnLockRead() throws JvnException {
 
-		// Accès séquentiel à l'objet
-		synchronized (this.state) {
+        // Accès séquentiel à l'objet
+        synchronized (this.state) {
 
-			boolean serverCall = false;
-			synchronized (this) {
-				if (this.state == JvnState.RC) {
-					this.state = JvnState.R;
-				} else if (this.state == JvnState.R) {
+            boolean serverCall = false;
+            synchronized (this) {
+                if (this.state == JvnState.RC) {
+                    this.state = JvnState.R;
+                } else if (this.state == JvnState.R) {
 
-				} else {
-					serverCall = true;
-				}
-			}
-			if (serverCall) {
-				Serializable o = JvnServerImpl.jvnGetServer().jvnLockRead(id);
+                } else {
+                    serverCall = true;
+                }
+            }
+            if (serverCall) {
+                Serializable o = JvnServerImpl.jvnGetServer().jvnLockRead(id);
+                this.state = R;
+                synchronized (this) {
+                    this.managedObject = o;
+                }
+            }
+        }
+    }
 
-				synchronized (this) {
-					this.managedObject = o;
-				}
-			}
-		}
-	}
+    @Override
+    public void jvnLockWrite() throws JvnException {
+        // Accès séquentiel à l'objet
+        synchronized (this.state) {
 
-	@Override
-	public void jvnLockWrite() throws JvnException {
-		// Accès séquentiel à l'objet
-		synchronized (this.state) {
+            boolean serverCall = false;
+            synchronized (this) {
+                if (this.state == JvnState.WC) {
+                    this.state = JvnState.W;
+                } else if (this.state == JvnState.W) {
 
-			boolean serverCall = false;
-			synchronized (this) {
-				if (this.state == JvnState.WC) {
-					this.state = JvnState.W;
-				} else if (this.state == JvnState.W) {
+                } else {
+                    serverCall = true;
+                }
+            }
+            if (serverCall) {
+                Serializable o = JvnServerImpl.jvnGetServer().jvnLockWrite(id);
+                this.state = R;
+                synchronized (this) {
+                    this.managedObject = o;
+                }
+            }
+        }
+    }
 
-				} else {
-					serverCall = true;
-				}
-			}
-			if (serverCall) {
-				Serializable o = JvnServerImpl.jvnGetServer().jvnLockWrite(id);
+    @Override
+    public void jvnUnLock() throws JvnException {
+        synchronized (this) {
+            if (this.state == JvnState.W) {
+                this.state = JvnState.WC;
+                this.notifyAll();
+            } else if (this.state == JvnState.R) {
+                this.state = JvnState.WC;
+                this.notifyAll();
+            } else {
 
-				synchronized (this) {
-					this.managedObject = o;
-				}
-			}
-		}
-	}
+                throw new JvnException("Unlock exception ");
+            }
 
-	@Override
-	public void jvnUnLock() throws JvnException {
-		synchronized (this) {
-			if (this.state == JvnState.W) {
-				this.state = JvnState.WC;
-				this.notifyAll();
-			} else if (this.state == JvnState.R) {
-				this.state = JvnState.WC;
-				this.notifyAll();
-			} else {
 
-				throw new JvnException("Unlock exception ");
-			}
-        
+        }
+    }
 
-		}
-	}
+    @Override
+    public int jvnGetObjectId() throws JvnException {
 
-	@Override
-	public int jvnGetObjectId() throws JvnException {
+        return this.id;
+    }
 
-		return this.id;
-	}
+    @Override
+    public Serializable jvnGetObjectState() throws JvnException {
+        return this.managedObject;
+    }
 
-	@Override
-	public Serializable jvnGetObjectState() throws JvnException {
-		return this.managedObject;
-	}
+    @Override
+    public void jvnInvalidateReader() throws JvnException {
+        // TODO InvalidateReader
 
-	@Override
-	public void jvnInvalidateReader() throws JvnException {
-		// TODO InvalidateReader
+    }
 
-	}
+    @Override
+    public Serializable jvnInvalidateWriter() throws JvnException {
+        // TODO InvalidateWriter
+        return null;
+    }
 
-	@Override
-	public Serializable jvnInvalidateWriter() throws JvnException {
-		// TODO InvalidateWriter
-		return null;
-	}
-
-	@Override
-	public Serializable jvnInvalidateWriterForReader() throws JvnException {
-		// TODO InvalidateWriterForReader
-		return null;
-	}
+    @Override
+    public Serializable jvnInvalidateWriterForReader() throws JvnException {
+        // TODO InvalidateWriterForReader
+        return null;
+    }
 
 }
