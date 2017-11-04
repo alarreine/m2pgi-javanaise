d7536d1336241e1231f8bff961426d3c61cacbb5
Agustin <alarreine@gmail.com>
2017-11-04 10:51:32 +0100

    Reorganisation of interfaces

---
+++ src/jvn/inter/JvnRemoteServer.java
@@ -0,0 +1,50 @@
+/***
+ * JAVANAISE API
+ * Contact: 
+ *
+ * Authors: 
+ */
+
+package jvn.inter;
+
+import jvn.exception.JvnException;
+
+import java.rmi.*;
+import java.io.*;
+
+
+/**
+ * Remote interface of a JVN server (used by a remote JvnCoord)
+ */
+
+public interface JvnRemoteServer extends Remote {
+	    
+	/**
+	* Invalidate the Read lock of a JVN object 
+	* @param joi : the JVN object id
+	* @throws java.rmi.RemoteException,JvnException
+	**/
+  public void jvnInvalidateReader(int joi)
+	throws java.rmi.RemoteException,JvnException;
+	    
+	/**
+	* Invalidate the Write lock of a JVN object 
+	* @param joi : the JVN object id
+	* @return the current JVN object state 
+	* @throws java.rmi.RemoteException,JvnException
+	**/
+        public Serializable jvnInvalidateWriter(int joi)
+	throws java.rmi.RemoteException,JvnException;
+	
+	/**
+	* Reduce the Write lock of a JVN object 
+	* @param joi : the JVN object id
+	* @return the current JVN object state
+	* @throws java.rmi.RemoteException,JvnException
+	**/
+   public Serializable jvnInvalidateWriterForReader(int joi)
+	 throws java.rmi.RemoteException,JvnException;
+
+}
+
+ 
