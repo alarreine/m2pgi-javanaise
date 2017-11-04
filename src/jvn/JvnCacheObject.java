0abfaedd867457028471cdb19e7c35c0def8a606
Agustin <alarreine@gmail.com>
2017-11-04 18:39:55 +0100

    Fix READ UNLOCK when a client try to read the modified object

--- src/jvn/JvnCacheObject.java
+++ src/jvn/JvnCacheObject.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
-public class JvnCacheObject {
+public class JvnCacheObject implements Serializable{
 
     private JvnObject object;
     private List<JvnRemoteServer> listClient;
